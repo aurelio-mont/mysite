2ab43a2878f949ee7f6b369f505fe429db43cc4e
aurelio <aurelio.mont25@gmail.com>
2018-12-30 21:48:28 -0600

    Agregando seccion admin

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('fecha de publicacion')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} de la pregunta {}'.format(self.choice_text, self.question.question_text)
--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hola, la pagina esta funcionando')
+    a = 2 + 3
+    return HttpResponse('Hola, la pagina esta funcionando {}'.format(a))
